9b92fdd8cd3cbe3722a35f723895765e99ee1667
craiglawsongit <S00209542@mail.itsligo.ie>
2024-10-23 12:09:27 +0100

    Initial project setup with Next.js

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Financial Dashboard</h1>
+      <p>Welcome to your financial dashboard!</p>
+    </div>
+  );
+};
+
+export default Home;
